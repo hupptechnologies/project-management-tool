c6d0131bf2e38c18cac9e4faefa768dbd5de91c7
hupptechnologies <git@hupp.in>
2025-05-06 12:30:30 +0530

    Set up ESLint and update the rules with a .prettierrc file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	prettierConfig,
+	{
+		// config with just ignores is the replacement for `.eslintignore`
+		ignores: ['**/build/**', '**/dist/**'],
+	},
+	{
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			quotes: ['error', 'single', { avoidEscape: true }],
+			semi: ['error', 'always'],
+			'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+			'prettier/prettier': ['error', { useTabs: true, tabWidth: 2 }],
+			'react-hooks/exhaustive-deps': 'off',
+			'no-console': ['error'],
+			'no-unused-vars': [
+				'error',
+				{
+					vars: 'all',
+					args: 'all',
+					ignoreRestSiblings: false,
+					argsIgnorePattern: '^_',
+				},
+			],
+			eqeqeq: 'error',
+			curly: 'error',
+			'no-duplicate-imports': 'error',
+		},
+	}
+);
